95bbb0c95f1248053fe310466d4659a8e72a9157
aimeelally <aimeelally@hotmail.com>
2018-06-29 04:16:36 +0700

    form now submits, wupwupwup, but theres loads of console errors, shucks

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,37 +1,19 @@
 import { Injectable } from '@angular/core';
 
-//import * as AWS from '../aws-sdk/dist/aws-sdk.min.js';
-
-// import entire SDK
-import AWS = require('../aws-sdk/dist/aws-sdk.min.js');
-
-// import AWS object without services
-// import AWS = require('./aws-sdk/global');
-// // import individual service
-// import S3 = require('aws-sdk/clients/s3');
-
-// declare global {
-//     var AWS: typeof AWSobj;
-// }
-
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ContactService {
 
-  /*<AWS.CognitoIdentityCredentials>AWS.config.credentials).identityId;*/
-
   sendMessage(form) {
-    debugger;
-    console.log(form);
+    
     var LPAWS = {};
 
     // Initialize the Amazon Cognito credentials provider
     AWS.config.region = 'us-east-1'; // Region
     
-    /*<AWS.CognitoIdentityCredentials>AWS.config.credentials; */
-
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({
         IdentityPoolId: 'us-east-1:54c6da73-e1de-481f-ad08-c29c7b3401b9',
     });
